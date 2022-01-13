db4618888787c77b0daacec286e9cb37f329b33e
Gergely Kiss-Illes <kissigergely@gmail.com>
2022-01-13 08:44:29 +0000

    wip: adds grid for statistics

---
+++ src/components/DoughnutChart.vue
@@ -0,0 +1,74 @@
+<template>
+  <apexchart
+      type="donut"
+      :options="chartOptions"
+      :series="series"
+      style="display: unset"
+      align="center"
+  ></apexchart>
+</template>
+
+<script>
+import VueApexCharts from "vue3-apexcharts";
+import store from '../store'
+
+export default {
+  components: {
+    apexchart: VueApexCharts,
+  },
+  props: {
+    correct: Number,
+    wrong: Number,
+    correctColor: String,
+    wrongColor: String
+  },
+  data: function() {
+    return {
+      // series: [store.state.correct, store.state.wrong],
+      series: [this.correct, this.wrong],
+      chartOptions: {
+        chart: {
+          width: 480,
+          expandOnClick: false
+        },
+        legend: {
+          //show: false,
+          position: 'bottom',
+        },
+        labels: ['Correct', 'Wrong'],
+        //colors: ['#32CD32', '#DC143C'],
+        colors: [this.correctColor, this.wrongColor],
+        responsive: [
+          {
+            breakpoint: 2000,
+            options: {
+              chart: {
+                width: 480,
+              },
+              legend: {
+                position: 'bottom',
+              }
+            }
+          },
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 300,
+              },
+              legend: {
+                position: 'bottom',
+              }
+            }
+          }
+        ],
+        plotOptions: {
+          pie: {
+            expandOnClick: false
+          },
+        }
+      }
+    };
+  },
+}
+</script>
--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,73 +1,55 @@
 <template>
   <div>
+    <h2>Overall</h2>
     <label>Correct answers: {{ $store.state.correct }}</label>
     <br />
     <label>Wrong answers: {{ $store.state.wrong }}</label>
-    <apexchart
-      type="donut"
-      :options="chartOptions"
-      :series="series"
-      style="display: unset"
-      align="center"
-    ></apexchart>
+    <doughnut-chart
+      v-if="$store.state.correct || $store.state.wrong"
+      :correct="$store.state.correct"
+      :wrong="$store.state.wrong"
+      :correctColor="'#32CD32'"
+      :wrongColor="'#DC143C'"
+    ></doughnut-chart>
+    <p v-else>No answers yet</p>
+
+    <h2>By Difficulty</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div v-for="(value, key) in $store.state.difficulties" :key="key">
+        <doughnut-chart
+          v-if="$store.state.correct || $store.state.wrong"
+          :correct="$store.state.difficulties[key].correct"
+          :wrong="$store.state.difficulties[key].wrong"
+          :correctColor="'#32CD32'"
+          :wrongColor="'#DC143C'"
+        ></doughnut-chart>
+        <p v-else>No answers yet</p>
+        {{ key }}
+      </div>      
+    </div>
+
+    <h2>By Category</h2>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div>01</div>
+      <!-- ... -->
+              <!-- {{ value.correct }}
+        <br />
+        {{ value.wrong }}
+        <br /> -->
+      <div>09</div>
+    </div>
   </div>
 </template>
 
 <script>
 import { Options, Vue } from "vue-class-component";
-import VueApexCharts from "vue3-apexcharts";
 import store from '../store'
+import DoughnutChart from '@/components/DoughnutChart.vue'
 
 @Options({
   components: {
-   apexchart: VueApexCharts,
-  },
-  data: function() {
-    return {
-      series: [store.state.correct, store.state.wrong],
-      chartOptions: {
-        chart: {
-          width: 480,
-          expandOnClick: false
-        },
-        legend: {
-          //show: false,
-          position: 'bottom',
-        },
-        labels: ['Correct', 'Wrong'],
-        colors: ['#32CD32', '#DC143C'],
-        responsive: [
-          {
-            breakpoint: 2000,
-            options: {
-              chart: {
-                width: 480,
-              },
-              legend: {
-                position: 'bottom',
-              }
-            }
-          },
-          {
-            breakpoint: 480,
-            options: {
-              chart: {
-                width: 300,
-              },
-              legend: {
-                position: 'bottom',
-              }
-            }
-          }
-        ],
-        plotOptions: {
-          pie: {
-            expandOnClick: false
-          },
-        }
-      }
-    };
-  },
+    DoughnutChart: DoughnutChart,
+  }
 })
 export default class Stats extends Vue {}
 </script>
